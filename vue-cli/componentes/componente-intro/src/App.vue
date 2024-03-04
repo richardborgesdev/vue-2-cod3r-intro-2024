b1f53450635cd6a09d96feeb1b467afa1a1c242a
Richard Borges <richard.borges.cc@gmail.com>
2024-03-04 10:22:39 -0300

    chore: create, use and register a global component

---
+++ vue-cli/componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      Contadores
+    </h1>
+    <contador-comp />
+    <contador-comp />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  }
+}
+</script>
