0ddf48a186e6aa20275c80e97195ade74d1a0939
Richard Borges <richard.borges.cc@gmail.com>
2024-04-12 14:14:57 -0300

    chore: solve form challenge

---
+++ formularios/formulario-desafio/src/components/NomeCompletoComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input type="text" name="" id="" :value="value.nome" @input="alterarNome">
+    </Rotulo>
+    <Rotulo nome="Sobrenome">
+      <input type="text" name="" id="" :value="value.sobrenome" @input="alterarSobrenome">
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from './RotuloComp.vue'
+
+export default { 
+  name: 'NomeCompletoComp',
+  components: { 
+    Rotulo, 
+  },
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    alterarNome(event) {
+      this.$emit(
+        'input', 
+        { 
+          nome: event.target.value, 
+          sobrenome: this.value.sobrenome, 
+        },
+      );
+    },
+    alterarSobrenome(event) {
+      this.$emit(
+        'input', 
+        { 
+          nome: this.value.name, 
+          sobrenome: event.target.value, 
+        },
+      );
+    },
+  }
+}
+</script>
