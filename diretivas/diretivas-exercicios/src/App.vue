6d0d796f430525ee37e7a7ddde1d113cc4324e0b
Richard Borges <richard.borges.cc@gmail.com>
2024-04-17 11:58:19 -0300

    chore: local directive and multiple modifiers

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -8,11 +8,53 @@
     <p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
     <p v-destaque="cor">Usando diretiva personalizada</p>
     <p v-teste:argumento.mod1.mod2.mod3="'valor'"></p>
+    <hr>
+    <p v-destaque-local:fundo.atrasar.alternar="'lightblue'">Usando diretiva personalizada</p>
+    <p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
 <script>
+
 export default {
+  directives: {
+    'destaque-local': {
+      bind(el, binding /*, vnode*/) {
+        const aplicarCor = cor => {
+          if (binding.arg === 'fundo') {
+            el.style.backgroundColor = cor;
+          } else {
+            el.style.color = cor;
+          }
+        };
+        let atraso = 0;
+
+        if (binding.modifiers['atrasar']) {
+          atraso = 3000;
+        }
+
+
+
+        const cor1 = binding.value;
+        const cor2 = 'purple';
+        let corAtual = cor1;
+
+        setTimeout(() => {
+          if (binding.modifiers['alternar']) {
+              setInterval(() => {
+                corAtual = corAtual === cor1
+                ? cor2
+                : cor1;
+
+                aplicarCor(corAtual);
+            }, 1000);
+          } else {
+            aplicarCor(binding.value);
+          }
+        }, atraso);
+      }
+    },
+  },
   data() {
     return {
       cor: 'red',
