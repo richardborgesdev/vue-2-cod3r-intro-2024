d67f44952c80cb3836d035161024565f2f471548
Richard Borges <richard.borges.cc@gmail.com>
2024-04-24 09:56:22 -0300

    chore: filters and mixins challenge resolution

--- filtros-mixins/filtros-mixins-desafio/src/main.js
+++ filtros-mixins/filtros-mixins-desafio/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter('contarPalavras', function(value) {
+  return value.split(' ').map(p => `${p} (${p.length})`).join(' ');
+})
 
 new Vue({
 	render: h => h(App),
