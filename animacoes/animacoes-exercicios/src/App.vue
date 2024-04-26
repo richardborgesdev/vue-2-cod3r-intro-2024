c3dd2d3638ffed09490a5d8ac6ccdf78ce0d3c1e
Richard Borges <richard.borges.cc@gmail.com>
2024-04-26 10:05:58 -0300

    chore: fade animation

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app" class="container-fluid">
     <h1>Animações</h1>
+    <hr>
+    <b-button variant="primary" class="mb-4" @click="exibir = !exibir">
+      Mostrar mensagem
+    </b-button>
+    <transition name="fade">
+      <b-alert variant="info" show v-if="exibir">
+        {{ msg }}
+      </b-alert>
+    </transition>
   </div>
 </template>
 
 <script>
 
 export default {
-
+  data() {
+    return {
+      msg: 'Uma mensagem de informação para o usuário',
+      exibir: false,
+    }
+  }
 }
 </script>
 
@@ -20,5 +34,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.fade-enter, .fade-leave-to { 
+  opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active { 
+  transition: opacity 2s;
 }
 </style>
